5ac9febb8bda0642d9c50a0df22d2bd86e981df4
Dariusz <rozik85@o2.pl>
2017-08-06 11:20:05 +0200

    1 zadanie

---
+++ src/main/java/Zadanie1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int x;
+
+        System.out.println("Podaj liczbę");
+        x = myScanner.nextInt();
+
+        int wynik = getAbsolute(x);
+        System.out.println("Wynik to: " + wynik);
+    }
+
+    public static int getAbsolute (int x) {
+        int wynik;
+
+
+        if (x >= 0) {
+            wynik = x;
+        } else {
+            wynik = x * -1;
+
+        }
+        return wynik;
+    }
+}
+
